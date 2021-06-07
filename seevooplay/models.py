2798b941413123f9aee9c05812aa6d9db7f8b9e8
Matthew Newton <matthewn@berkeley.edu>
2021-06-06 17:16:27 -0700

    build out initial version of models

--- seevooplay/models.py
+++ seevooplay/models.py
@@ -1,3 +1,54 @@
+from django.conf import settings
 from django.db import models
+from model_utils.models import TimeStampedModel, UUIDModel
 
-# Create your models here.
+
+class Guest(TimeStampedModel, UUIDModel):
+    name = models.CharField(max_length=64)
+    email = models.EmailField()
+
+
+class Event(TimeStampedModel):
+    name = models.CharField(max_length=64)
+    host = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField(blank=True)
+    location_name = models.CharField(max_length=64)
+    location_address = models.CharField(
+        blank=True,
+        max_length=128,
+    )
+    details = models.TextField(blank=True)
+    image = models.ImageField(
+        blank=True,
+        upload_to='event_images/',
+        height_field='image_height',
+        width_field='image_width',
+    )
+    image_height = models.PositiveIntegerField(blank=True)
+    image_width = models.PositiveIntegerField(blank=True)
+    guests = models.ManyToManyField(Guest, blank=True)
+
+
+class ReplyStatus(models.TextChoices):
+    YES = 'Y', 'Yes'
+    NO = 'N', 'No'
+    MAYBE = 'M', 'Maybe'
+
+
+class Reply(TimeStampedModel):
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+    )
+    guest = models.ForeignKey(
+        Guest,
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=ReplyStatus.choices,
+    )
