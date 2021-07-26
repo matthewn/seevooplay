f42ccd7945823f21d971d9d1d3331b90826b8c1a
Matthew Newton <matthewn@berkeley.edu>
2021-07-25 17:00:45 -0700

    get lookups for email_guests processing working

--- seevooplay/views.py
+++ seevooplay/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 
 from .forms import EmailGuestsForm
+from .models import Event, Reply
 
 
 @staff_member_required
@@ -16,13 +17,52 @@ def email_guests(request, event_id):
         form = EmailGuestsForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+            event = Event.objects.get(id=event_id)
+            people_to_email = []
+
+            if form.cleaned_data['want_reply_yes']:
+                replies = Reply.objects.filter(event=event, status='Y')
+                people_to_email.append([reply.guest for reply in replies])
+
+            if form.cleaned_data['want_reply_no']:
+                replies = Reply.objects.filter(event=event, status='N')
+                people_to_email.append([reply.guest for reply in replies])
+
+            if form.cleaned_data['want_reply_maybe']:
+                replies = Reply.objects.filter(event=event, status='M')
+                people_to_email.append([reply.guest for reply in replies])
+
+            if form.cleaned_data['want_reply_none']:
+                replies = Reply.objects.filter(event=event, status='')
+                people_to_email.append([reply.guest for reply in replies])
+
+            if form.cleaned_data['want_have_viewed']:
+                replies = Reply.objects.filter(event=event, has_viewed=True)
+                people_to_email.append([reply.guest for reply in replies])
+
+            if form.cleaned_data['want_have_not_viewed']:
+                replies = Reply.objects.filter(event=event, has_viewed=False)
+                people_to_email.append([reply.guest for reply in replies])
+
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            # flatten our list of lists
+            people_to_email = [item for sublist in people_to_email for item in sublist]
+            # dedupe the list
+            people_to_email = list(set(people_to_email))
+
+            print(f'{subject} {message}')
+
             messages.add_message(
                 request,
                 messages.INFO,
-                'FOOOO!',
+                f'event id is {event}',
+            )
+            messages.add_message(
+                request,
+                messages.INFO,
+                f'people to email are {people_to_email}',
             )
             return HttpResponseRedirect(
                 reverse('admin:seevooplay_event_change', args=(event_id,))
