c10f17f93efbb61fbf23545f893943000fda55e5
Matthew Newton <matthewn@mahnamahna.net>
2025-07-28 18:13:12 -0700

    add view tests

--- seevooplay/tests/conftest.py
+++ seevooplay/tests/conftest.py
@@ -1,24 +1,31 @@
 from zoneinfo import ZoneInfo
-from seevooplay.models import Event, Guest
+from seevooplay.models import Event, Guest, Reply
 
 import datetime as dt
 import pytest
 
 
-@pytest.fixture
-def guest1(db):
-    guest1 = Guest.objects.create(name='Guest One', email='guest1@example.org')
-    guest1.save()
-    yield guest1
-    guest1.delete()
+NUMBERS = [
+    'One', 'Two', 'Three', 'Four', 'Five', 'Six',
+    'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve'
+]
 
 
-@pytest.fixture
-def guest2(db):
-    guest2 = Guest.objects.create(name='Guest Two', email='guest2@example.org')
-    guest2.save()
-    yield guest2
-    guest2.delete()
+def create_guest_fixture(n):
+    @pytest.fixture
+    def guest_fixture(db):
+        guest = Guest.objects.create(
+            name=f'Guest {NUMBERS[n - 1]}',
+            email=f'guest{n}@example.org'
+        )
+        yield guest
+        guest.delete()
+    return guest_fixture
+
+
+# make guest1 through guest12 available to all tests
+for i in range(1, 13):
+    globals()[f'guest{i}'] = create_guest_fixture(i)
 
 
 @pytest.fixture
@@ -36,4 +43,82 @@ def event(db, guest1, guest2):
     event.guests.add(guest1, guest2)
     event.save()
     yield event
+    Reply.objects.filter(event=event).delete()
     event.delete()
+
+
+@pytest.fixture
+def past_event(db, guest1, guest2):
+    event = Event.objects.create(
+        name='Past Event',
+        host1_name='Groovy',
+        host1_email='groovy@example.org',
+        invitees='Guest One guest1@example.org, Guest Two guest2@example.org',
+        start_datetime=dt.datetime(
+            2020, 1, 1, 12, tzinfo=ZoneInfo('America/Los_Angeles')
+        ),
+        location_name='Fake Location',
+    )
+    event.guests.add(guest1, guest2)
+    event.save()
+    yield event
+    Reply.objects.filter(event=event).delete()
+    event.delete()
+
+
+@pytest.fixture
+def big_event(
+    event,
+    guest1, guest2, guest3, guest4, guest5, guest6,
+    guest7, guest8, guest9, guest10, guest11, guest12,
+):
+    # guests 1 and 2 are built-in to event; we add the rest here
+    # (adding guests creates Reply objects)
+    event.guests.add(
+        guest3, guest4, guest5, guest6, guest7, guest8,
+        guest9, guest10, guest11, guest12
+    )
+    # set some varying replies
+    reply_guest1 = Reply.objects.get(guest=guest1)
+    reply_guest1.status = 'Y'
+    reply_guest1.has_viewed = True
+    reply_guest1.save()
+    reply_guest2 = Reply.objects.get(guest=guest2)
+    reply_guest2.status = 'Y'
+    reply_guest2.has_viewed = True
+    reply_guest2.save()
+    reply_guest3 = Reply.objects.get(guest=guest3)
+    reply_guest3.status = 'Y'
+    reply_guest3.has_viewed = True
+    reply_guest3.save()
+    reply_guest4 = Reply.objects.get(guest=guest4)
+    reply_guest4.status = 'Y'
+    reply_guest4.has_viewed = True
+    reply_guest4.save()
+    reply_guest5 = Reply.objects.get(guest=guest5)
+    reply_guest5.status = 'M'
+    reply_guest5.has_viewed = True
+    reply_guest5.save()
+    reply_guest6 = Reply.objects.get(guest=guest6)
+    reply_guest6.status = 'M'
+    reply_guest6.has_viewed = True
+    reply_guest6.save()
+    reply_guest7 = Reply.objects.get(guest=guest7)
+    reply_guest7.status = 'M'
+    reply_guest7.has_viewed = True
+    reply_guest7.save()
+    reply_guest8 = Reply.objects.get(guest=guest8)
+    reply_guest8.status = 'N'
+    reply_guest8.has_viewed = True
+    reply_guest8.save()
+    reply_guest9 = Reply.objects.get(guest=guest9)
+    reply_guest9.status = 'N'
+    reply_guest9.has_viewed = True
+    reply_guest9.save()
+    # guest10 has viewed, but not replied
+    reply_guest10 = Reply.objects.get(guest=guest10)
+    reply_guest10.has_viewed = True
+    reply_guest10.save()
+    # guests 11 and 12 have not viewed, not replied
+    return event
+    # cleanup is handled by parent fixtures
---
+++ seevooplay/tests/test_views.py
@@ -0,0 +1,180 @@
+import re
+
+
+def test_event_page(event, guest1, client):
+    response = client.get(f'/rsvp/1/{guest1.id}/')
+    assert response.status_code == 200
+    assert 'No Reply: 2' in response.content.decode()
+
+
+def test_event_page_admin_access(event, admin_client):
+    response = admin_client.get('/rsvp/1/')
+    assert response.status_code == 200
+    assert 'No Reply: 2' in response.content.decode()
+
+
+def test_event_page_no_snooping(event, client):
+    response = client.get('/rsvp/1/')
+    assert response.status_code == 403
+
+
+def test_event_page_reply_yes(event, guest1, client, mailoutbox):
+    response = client.post(
+        f'/rsvp/1/{guest1.id}/',
+        {
+            'status': 'Y',
+            'extra_guests': '1',
+            'comment': 'WOOOO!',
+        },
+    )
+    assert response.status_code == 200
+    html = response.content.decode()
+    assert 'Thank you for your reply!' in html
+    assert 'Yes: 2' in html
+    assert 'No Reply: 1' in html
+    assert '<div class="comment">WOOOO!</div>' in html
+    assert len(mailoutbox) == 1
+
+
+def test_event_page_reply_no(event, guest1, client, mailoutbox):
+    response = client.post(
+        f'/rsvp/1/{guest1.id}/',
+        {
+            'status': 'N',
+            'extra_guests': '0',
+        },
+    )
+    assert response.status_code == 200
+    html = response.content.decode()
+    assert 'We will miss you!' in html
+    assert 'No: 1' in html
+    assert 'No Reply: 1' in html
+    assert len(mailoutbox) == 1
+
+
+def test_event_page_reply_maybe(event, guest1, client, mailoutbox):
+    response = client.post(
+        f'/rsvp/1/{guest1.id}/',
+        {
+            'status': 'M',
+            'extra_guests': '3',
+        },
+    )
+    assert response.status_code == 200
+    html = response.content.decode()
+    assert 'We hope you can make it!' in html
+    assert 'Maybe: 4' in html
+    assert 'No Reply: 1' in html
+    assert len(mailoutbox) == 1
+
+
+def test_resend_emails(event, guest1, client, mailoutbox):
+    response = client.post('/', {'email': 'guest1@example.org'})
+    assert len(mailoutbox) == 1
+    assert (
+        'Hey there, Guest One! There&#x27;s email headed your way.'
+        in response.content.decode()
+    )
+
+
+def test_resend_emails_unknown(event, client, mailoutbox):
+    response = client.post('/', {'email': 'foo@example.org'})
+    assert len(mailoutbox) == 0
+    assert (
+        'Sorry, foo@example.org is not in our records'
+        in response.content.decode()
+    )
+
+
+def test_resend_emails_past_event(past_event, guest1, client, mailoutbox):
+    response = client.post('/', {'email': 'guest1@example.org'})
+    assert len(mailoutbox) == 0
+    assert (
+        'Sorry, no outstanding invites for you, Guest One.'
+        in response.content.decode()
+    )
+
+
+def process_test_email_response(response, admin_client):
+    """
+    Helper function for the next four tests.
+    """
+    assert response.status_code == 302  # redirect after successful save
+    response = admin_client.get(response.url)  # fetch content of page we got redirected to
+    pattern = r'<li class="info">Email sent to:\s*([^<]+)</li>'
+    match = re.search(pattern, response.content.decode())
+    email_display_count = 0
+    if match:
+        email_list = match.group(1)
+        email_display_count = len([email.strip() for email in email_list.split(',') if email.strip()])
+        return email_display_count
+
+
+def test_email_guests_1(big_event, admin_client, mailoutbox):
+    response = admin_client.post(
+        f'/admin/email_guests/{big_event.id}/',
+        {
+            'want_reply_yes': ['on'],
+            'want_reply_maybe': ['on'],
+            'want_reply_no': ['on'],
+            'want_reply_none': ['on'],
+            'want_have_viewed': ['on'],
+            'want_have_not_viewed': ['on'],
+            'subject': ['foo'],
+            'message': ['bar'],
+        },
+    )
+    email_display_count = process_test_email_response(response, admin_client)
+    assert email_display_count == 12
+    assert len(mailoutbox) == 12
+
+
+def test_email_guests_2(big_event, admin_client, mailoutbox):
+    response = admin_client.post(
+        f'/admin/email_guests/{big_event.id}/',
+        {
+            'want_reply_maybe': ['on'],
+            'want_reply_no': ['on'],
+            'want_reply_none': ['on'],
+            'want_have_viewed': ['on'],
+            'want_have_not_viewed': ['on'],
+            'subject': ['foo'],
+            'message': ['bar'],
+        },
+    )
+    email_display_count = process_test_email_response(response, admin_client)
+    assert email_display_count == 8
+    assert len(mailoutbox) == 8
+
+
+def test_email_guests_3(big_event, admin_client, mailoutbox):
+    response = admin_client.post(
+        f'/admin/email_guests/{big_event.id}/',
+        {
+            'want_reply_none': ['on'],
+            'want_have_not_viewed': ['on'],
+            'subject': ['foo'],
+            'message': ['bar'],
+        },
+    )
+    email_display_count = process_test_email_response(response, admin_client)
+    assert email_display_count == 2
+    assert len(mailoutbox) == 2
+
+
+def test_email_guests_4(big_event, admin_client, mailoutbox):
+    response = admin_client.post(
+        f'/admin/email_guests/{big_event.id}/',
+        {
+            'want_reply_yes': ['on'],
+            'want_reply_maybe': ['on'],
+            'want_reply_no': ['on'],
+            'want_reply_none': ['on'],
+            'want_have_viewed': ['on'],
+            'subject': ['foo'],
+            'message': ['bar'],
+        },
+    )
+    email_display_count = process_test_email_response(response, admin_client)
+    assert email_display_count == 10
+    assert len(mailoutbox) == 10
